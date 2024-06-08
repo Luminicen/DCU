6187ea706ebe3fe31b612681b831ec5df6d8ca66
Juliana Luminicen Delle Ville <julianadelleville@hotmail.com>
2024-06-08 20:08:35 -0300

    gitignore

--- accesibilidadIA/accesibilidadIA/settings.py
+++ accesibilidadIA/accesibilidadIA/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "Templates"],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
