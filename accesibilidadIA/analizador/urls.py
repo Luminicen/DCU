7d04d8daa1d4beb3fd17eb4e3d5e710933ef9443
Nela <diazcatanm@gmail.com>
2024-06-09 18:13:11 -0300

    agrega menu vertical

--- accesibilidadIA/analizador/urls.py
+++ accesibilidadIA/analizador/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("registro/", views.register, name="register"),
+    path("analysis", views.analysis, name="analysis"),
+    path("results", views.results, name="results"),
+    path("settings", views.settings, name="settings"),
 ]
