c2f9f027833b15c8685f55f5e4d2501754b7db99
Juliana Luminicen Delle Ville <julianadelleville@hotmail.com>
2024-06-09 16:18:08 -0300

    form basico

--- accesibilidadIA/analizador/views.py
+++ accesibilidadIA/analizador/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.http import HttpResponse
-
+from django.contrib.auth import login, authenticate
+from .forms import Registro
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def register(request):
+    if request.method == 'POST':
+        form = Registro(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()  # Load the profile instance created by the signal
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = Registro()
+    return render(request, 'registration/registerForm.html', {'form': form})
