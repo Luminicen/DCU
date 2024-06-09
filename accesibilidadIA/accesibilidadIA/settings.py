b51ef4dd51612abc9f4b0db3a7366ac08c605ce8
Juliana Luminicen Delle Ville <julianadelleville@hotmail.com>
2024-06-09 15:45:38 -0300

    Update settings.py

--- accesibilidadIA/accesibilidadIA/settings.py
+++ accesibilidadIA/accesibilidadIA/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     }
 }
 
-LOGIN_REDIRECT_URL = 'login'
+LOGIN_REDIRECT_URL = 'index'
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
