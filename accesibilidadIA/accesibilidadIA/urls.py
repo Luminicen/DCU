9622f957bedaed2835aa56b9cf358018a28fa5d3
nahuelherpo <nahuelherpo@gmail.com>
2024-06-15 13:34:33 -0300

    added basic configuration template

--- accesibilidadIA/accesibilidadIA/urls.py
+++ accesibilidadIA/accesibilidadIA/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("analizador/", include("analizador.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- accesibilidadIA/analizador/apps.py
+++ accesibilidadIA/analizador/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AnalizadorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'analizador'
+
+class Settings(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'settings'
