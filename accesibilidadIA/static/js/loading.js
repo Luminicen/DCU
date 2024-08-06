e9e428dc4c308b35b11c29e753ae1a52075addf7
Nela <diazcatanm@gmail.com>
2024-08-06 10:47:28 -0300

    menu

--- accesibilidadIA/static/js/loading.js
+++ accesibilidadIA/static/js/loading.js
@@ -4,17 +4,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if (submitBtn && form) {
         submitBtn.addEventListener('click', function(event) {
-            // Evitar el envío inmediato del formulario
             event.preventDefault();
 
-            // Mostrar el indicador de carga
             document.querySelector('.loading').style.display = 'flex';
             document.querySelector('.loading-text').style.display = 'block';
             
-            // Esperar un poco antes de enviar el formulario para mostrar el indicador
             setTimeout(function() {
                 form.submit();
-            }, 100); // Ajusta el tiempo según sea necesario
+            }, 100); 
         });
     }
 });
