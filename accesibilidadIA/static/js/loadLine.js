54478586c7c32e4556a63a17ff6776a7c9f4d5d2
Nela <diazcatanm@gmail.com>
2025-01-13 19:48:32 -0300

    sol responsive cuenta

--- accesibilidadIA/static/js/loadLine.js
+++ accesibilidadIA/static/js/loadLine.js
@@ -1,10 +1,10 @@
-function redirigir(analysis_id, file_name, error_name) {
+function redirigir(analysis_id, analysis_name, file_name, error_name) {
     const loadingModal = document.getElementById('loading-modal');
     console.log("On loading view")
     event.preventDefault(); 
     loadingModal.style.display = 'flex'; 
     loadingModal.setAttribute('aria-hidden', 'false');
-    window.location.href = `/analizador/error_result/${analysis_id}/${file_name}/${error_name}`;
+    window.location.href = `/analizador/error_result/${analysis_id}/${analysis_name}/${file_name}/${error_name}`;
 }
 
 function mergeLine(analysis_id, error_name) {
@@ -23,3 +23,4 @@ function backToResults() {
         window.location.href = `/results`;
     }
 }
+
